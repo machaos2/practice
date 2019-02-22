a0280dde7a381e77a0d44e07419eb95dac12e3aa
machao <2653406590@qq.com>
2019-02-22 14:16:50 +0800

    20190222

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+  	<Header/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Header from'./components/Header'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
 <style>
+	*{margin: 0;padding: 0;box-sizing: border-box;}
+	html,body{font-family:"Helvetica Neue", NotoSansHans-Regular,AvenirNext-Regular,arial,Hiragino Sans GB,"Microsoft Yahei","Hiragino Sans GB","WenQuanYi Micro Hei",sans-serif;}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+.mainwrap{width: 1200px;margin: 0 auto;}
+.clr:after {display: block;content: " ";clear: both;height: 0;}
+.clr {zoom: 1;}
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+	<div class="header">
+		<div class="navs mainwrap">
+			<div class="topleft">
+				<img src="../assets/logo.png">
+				<div class="toptxt">Vue.js</div>
+				<div class="arrowdown">
+					<i></i>
+				</div>
+				<div class="topmenu">
+					
+				</div>
+			</div>
+		</div>
+		
+	</div>
+</template>
+
+<script>
+	export default{
+		name:'Header'
+	}
+</script>
+
+<style scoped lang="scss">
+	.header{border-top:4px solid #4fc08d;height:auto;height:54px;box-shadow: 0 0 10px 0 rgba(42,42,42,0.1);
+		.navs{height:50px;
+			.topleft{float: left;display: flex;height: 100%;align-items: center;cursor: pointer;
+				img{width: 26px;height: 26px;margin-right: 10px;}
+				.toptxt{float: right;color: #888;font-size: 14px;font-weight: bold;}
+				.arrowdown{margin-left: 15px;
+					i{display: inline-block;width: 0; height: 0;border-left: 5px solid transparent;border-right: 5px solid transparent;border-top: 6px solid #666;}
+				}
+				.topmenu{overflow-x: hidden;overflow-y: auto;
+					
+				}
+			}
+		}
+		
+	}
+</style>
